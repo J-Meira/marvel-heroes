a4f57fee7c528c96d2da271aa9cbcd97745a0244
J-Meira <jonathanmeira@hotmail.com>
2023-07-02 16:30:41 -0300

    done

---
+++ src/pages/About/index.tsx
@@ -0,0 +1,86 @@
+import moment from 'moment';
+import { Grid, Typography } from '@mui/material';
+
+const date = new Date();
+
+export const AboutPage = () => {
+  const getReleaseDate = () => {
+    const date = moment(
+      process.env.REACT_APP_V_DATE || '2023-07-01T21:00:00',
+    );
+
+    return `${date.format('MM/DD/YYYY')} - ${date
+      .subtract(3, 'hours')
+      .format('hh:mm A')}`;
+  };
+
+  return (
+    <Grid container className='about-page'>
+      <Grid item xs={12}>
+        <Typography variant='h2' className='page-title'>
+          Marvel Heros List in{' '}
+          <a
+            href='https://react.dev'
+            rel='noopener noreferrer'
+            target='_blank'
+          >
+            React
+          </a>
+        </Typography>
+        <Typography variant='h4' className='app-version'>
+          {process.env.REACT_APP_VERSION} of {getReleaseDate()}
+        </Typography>
+        <Typography variant='body1' className='description'>
+          {'Developer by '}
+          <a
+            href='https://github.com/J-Meira'
+            rel='noopener noreferrer'
+            target='_blank'
+          >
+            J-Meira
+          </a>
+          {' with data provided by '}
+          <a
+            href='https://marvel.com'
+            rel='noopener noreferrer'
+            target='_blank'
+          >
+            © {date.getFullYear()} MARVEL
+          </a>
+        </Typography>
+        <Typography variant='body1' className='description'>
+          <b>Using:</b>
+          <ul>
+            <li>
+              <a
+                href='https://mui-theme.jm.app.br'
+                rel='noopener noreferrer'
+                target='_blank'
+              >
+                Mui Theme;
+              </a>
+            </li>
+            <li>
+              <a
+                href='https://react.dev/reference/react/useContext'
+                rel='noopener noreferrer'
+                target='_blank'
+              >
+                React Context;
+              </a>
+            </li>
+            <li>
+              <a
+                href='https://redux.js.org'
+                rel='noopener noreferrer'
+                target='_blank'
+              >
+                Redux;
+              </a>
+            </li>
+          </ul>
+        </Typography>
+      </Grid>
+    </Grid>
+  );
+};
