ad4df0686fb21fa2ba2aca1982b95c9c9a0df118
J-Meira <jonathanmeira@hotmail.com>
2023-07-02 01:04:35 -0300

    done

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,9 +4,9 @@ import { ContextPage, HomePage, NotFoundPage, ReduxPage } from '../pages';
 export const AppRoutes = () => (
   <Routes>
     <Route path='/context' element={<ContextPage />} />
-    <Route path='/context/:charId' element={<ContextPage />} />
+    <Route path='/context/:id' element={<ContextPage />} />
     <Route path='/redux' element={<ReduxPage />} />
-    <Route path='/redux/:charId' element={<ReduxPage />} />
+    <Route path='/redux/:id' element={<ReduxPage />} />
     <Route path='/' element={<HomePage />} />
     <Route path='*' element={<NotFoundPage />} />
   </Routes>
