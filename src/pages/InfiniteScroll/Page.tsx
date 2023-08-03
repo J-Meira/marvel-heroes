7d0f341c270f2cc92c5d3a577df093a21fc38b16
J-Meira <jonathanmeira@hotmail.com>
2023-08-03 14:33:57 -0300

    done

---
+++ src/pages/InfiniteScroll/Page.tsx
@@ -0,0 +1,255 @@
+import { useEffect, useRef, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+
+import 'intersection-observer';
+
+import { Box, Grid, Typography } from '@mui/material';
+import { Search as SearchIcon } from '@mui/icons-material';
+import {
+  Button,
+  Input,
+  useDebounce,
+  useMultiContext,
+  useWindowDimensions,
+} from '@j-meira/mui-theme';
+
+import {
+  CharacterCard,
+  CharacterDetails,
+  Loading,
+  orberByList,
+} from '../../components';
+
+import { useLoadingContext } from '../../contexts';
+import { CharactersService } from '../../services';
+import { ICharacter, IGetAll } from '../../types';
+
+const params: IGetAll = {
+  limit: 20,
+  offset: 0,
+  orderBy: 'name',
+  nameStartsWith: undefined,
+};
+
+export const Page = () => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const observerTarget = useRef(null);
+  const { debounce } = useDebounce(500, true);
+  const { backgroundColor } = useMultiContext();
+  const { width } = useWindowDimensions();
+  const { isLoading, setLoading, removeLoading } = useLoadingContext();
+  const [search, setSearch] = useState('');
+  const [orderBy, setOrderBy] = useState(1);
+  const [offset, setOffset] = useState(0);
+  const [local, setLocal] = useState(false);
+  const [characters, setCharacters] = useState<ICharacter[]>([]);
+  const [totalOfCharacters, setTotalOfCharacters] = useState(0);
+  const [character, setCharacter] = useState<ICharacter | undefined>(
+    undefined,
+  );
+  const [open, setOpen] = useState(false);
+  const [mobile, setMobile] = useState(false);
+  const [hide, setHide] = useState(false);
+
+  const getCharacters = (params: IGetAll, more: boolean) => {
+    setLoading();
+    setLocal(more);
+    CharactersService.getAll(params).then((result) => {
+      setLocal(false);
+      removeLoading();
+      if (result) {
+        if (more) {
+          setCharacters((prevCharacters) => [
+            ...prevCharacters,
+            ...result.data.results,
+          ]);
+        } else {
+          setCharacters(result.data.results);
+          setTotalOfCharacters(result.data.total);
+        }
+      }
+    });
+  };
+
+  const getMoreCharacters = (scrollTigger: boolean) => {
+    if (scrollTigger && (isLoading || totalOfCharacters === 0))
+      return null;
+
+    const newOffset = offset + 20;
+    setOffset(newOffset);
+
+    return getCharacters({ ...params, offset: newOffset }, true);
+  };
+
+  const openDetail = (paramId: number) => {
+    setLoading();
+    CharactersService.getById(paramId).then((result) => {
+      removeLoading();
+      if (result && result.data.results[0]) {
+        setCharacter(result.data.results[0]);
+      }
+    });
+  };
+
+  const closeDetail = () => {
+    setOpen(false);
+    setCharacter(undefined);
+    if (id) navigate('/infinite-scroll');
+  };
+
+  useEffect(() => {
+    debounce(() => {
+      const test = orberByList.find((o) => o.value === orderBy);
+      getCharacters(
+        {
+          ...params,
+          orderBy: test?.obj.apiValue,
+          nameStartsWith: search !== '' ? search : undefined,
+        },
+        false,
+      );
+    });
+
+    // eslint-disable-next-line
+  }, [orderBy, search]);
+
+  useEffect(() => {
+    if (character) setOpen(true);
+
+    // eslint-disable-next-line
+  }, [character]);
+
+  useEffect(() => {
+    debounce(() => {
+      if (id) {
+        openDetail(Number(id));
+      }
+    });
+
+    // eslint-disable-next-line
+  }, [id]);
+
+  useEffect(() => {
+    setMobile(width < 840);
+
+    // eslint-disable-next-line
+  }, [width]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting && offset < totalOfCharacters) {
+          getMoreCharacters(true);
+          observer.unobserve(entries[0].target);
+          setHide(false);
+        } else if (offset >= totalOfCharacters) {
+          setHide(true);
+        }
+      },
+      {
+        root: null,
+        rootMargin: '0px',
+        threshold: 1.0,
+      },
+    );
+    if (observerTarget.current) {
+      observer.observe(observerTarget.current);
+    }
+
+    return () => {
+      if (observerTarget.current) {
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        observer.unobserve(observerTarget.current);
+      }
+    };
+
+    // eslint-disable-next-line
+  }, [isLoading, totalOfCharacters]);
+
+  return (
+    <Box className='infinite-scroll'>
+      <Grid
+        sx={{
+          backgroundColor: backgroundColor,
+        }}
+        container
+        className='actions'
+        spacing={2}
+      >
+        <Input
+          autoFocus
+          label='Search'
+          model='icon'
+          grid={
+            mobile
+              ? { xs: 8, sm: 8, md: 8, lg: 8 }
+              : { xs: 6, sm: 6, md: 6, lg: 6 }
+          }
+          icon={<SearchIcon />}
+          isNoFormik
+          className='search'
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <Input
+          label='Order by'
+          model='select'
+          grid={{ xs: 4, sm: 4, md: 4, lg: 4 }}
+          isNoFormik
+          className='order'
+          options={orberByList}
+          value={orderBy}
+          onChange={(e) => setOrderBy(Number(e.target.value))}
+        />
+        {!mobile && (
+          <Grid item xs={2}>
+            <Typography variant='caption'>
+              {`${totalOfCharacters > 0 ? 1 : 0}-${characters.length}
+          of ${totalOfCharacters}`}
+            </Typography>
+          </Grid>
+        )}
+      </Grid>
+      <Grid container justifyContent='center' className='list'>
+        {characters.map((character, i) => (
+          <CharacterCard
+            key={i}
+            data={character}
+            openDetail={(id) => openDetail(id)}
+          />
+        ))}
+        <Grid item xs={12} className='list-footer' ref={observerTarget}>
+          {characters.length === 0 && search !== '' && (
+            <Typography variant='caption'>No cards to display.</Typography>
+          )}
+          {isLoading && !hide && (
+            <Typography variant='caption'>Loading...</Typography>
+          )}
+          {hide && (
+            <Typography variant='caption'>
+              No more cards to display.
+            </Typography>
+          )}
+          {!isLoading && !hide && (
+            <Button
+              size='large'
+              onClick={() => getMoreCharacters(false)}
+              fullWidth={false}
+            >
+              Load More
+            </Button>
+          )}
+        </Grid>
+      </Grid>
+      <Loading isLoading={isLoading && !local} />
+      {character && (
+        <CharacterDetails
+          open={open}
+          toggle={closeDetail}
+          data={character}
+        />
+      )}
+    </Box>
+  );
+};
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,6 @@
 export * from './About';
 export * from './Context';
+export * from './InfiniteScroll';
 export * from './Home';
 export * from './NotFound';
 export * from './Redux';
